016ba1319b010861ecfc72e0a21510b8f615fdd8
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2010-07-24 18:49:18 +0300

    Implement post title step

--- features/step_definitions/front_page_steps.rb
+++ features/step_definitions/front_page_steps.rb
@@ -2,8 +2,9 @@ Given /^I have a post$/ do
   @post = Post.create
 end
 
-Given /^my post has title "([^"]*)"$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+Given /^my post has title "([^"]*)"$/ do |title|
+  @post[:title] = title
+  @post.save
 end
 
 Given /^my post has body "([^"]*)"$/ do |arg1|

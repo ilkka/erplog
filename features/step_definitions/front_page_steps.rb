17d1feca848d356a3d32c3de315b05d1773bdc06
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2010-07-24 18:51:37 +0300

    Implemented publication date step

--- features/step_definitions/front_page_steps.rb
+++ features/step_definitions/front_page_steps.rb
@@ -12,10 +12,11 @@ Given /^my post has body "([^"]*)"$/ do |body|
   @post.save
 end
 
-Given /^my post has author with name "([^"]*)"$/ do |arg1|
+Given /^my post has author with name "([^"]*)"$/ do |author|
   pending # express the regexp above with the code you wish you had
 end
 
-Given /^my post was published on "([^"]*)"$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+Given /^my post was published on "([^"]*)"$/ do |pubdate|
+  @post[:published_on] = pubdate
+  @post.save
 end

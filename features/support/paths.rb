41348629787ee5e5d793aed25f46d1ea98667dbb
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2010-07-26 00:50:25 +0300

    Add path mapping for post creation page for cucumber

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -11,6 +11,9 @@ module NavigationHelpers
     when /the home\s?page/
       '/'
 
+    when /the post creation page/
+      '/posts/new'
+      
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
     #

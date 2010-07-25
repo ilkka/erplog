4e84a00d0f13b088b94470821ee180536fbc4a08
Ilkka Laukkanen <ilkka.s.laukkanen@gmail.com>
2010-07-26 00:54:57 +0300

    Remove unnecessary step definition skeleton

--- features/step_definitions/post_steps.rb
+++ features/step_definitions/post_steps.rb
@@ -5,7 +5,3 @@ end
 Given /^I have a user named "([^"]*)"$/ do |name|
   User.create(:name => name)
 end
-
-When /^I click "([^"]*)"$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
-end

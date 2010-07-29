21159fb11662bbb2fdc71326cca79266720f1b31
Ilkka Laukkanen <ilkka.laukkanen@sasken.com>
2010-07-29 15:09:08 +0300

    Replace create() with new() in post creation to avoid unnecessary double save

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,7 +12,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.create(params[:post])
+    @post = Post.new(params[:post])
     if @post.save
       redirect_to post_url(@post.id)
     else
